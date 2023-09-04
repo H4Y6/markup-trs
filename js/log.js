13865c85bc602937f20082f9d40d4c7f100925ec
h4y6 <h4y6@i.ua>
2023-09-04 16:50:21 +0300

    modified:   contacts.html modified:   css/style-c.css modified:   css/style-f.css modified:   css/styles.css modified:   forms.html modified:   img/sprite.svg modified:   js/log.js

--- js/log.js
+++ js/log.js
@@ -1,6 +1,5 @@
 (() => {
-    document.querySelector('.js-speaker-form')
-        // document.querySelector('.js-contact-form')
+    document.querySelector('.js-form')
         .addEventListener('submit', e => {
             e.preventDefault();
             new FormData(e.currentTarget).forEach((value, name) =>
