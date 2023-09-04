254e1a57e1f28ee9f13ae992b7386553a7cfda8f
h4y6 <h4y6@i.ua>
2023-09-04 16:49:41 +0300

    modified:   contacts.html modified:   css/style-c.css modified:   img/img-c/airplane.svg modified:   img/sprite.svg new file:   js/log.js

---
+++ js/log.js
@@ -0,0 +1,10 @@
+(() => {
+    document.querySelector('.js-speaker-form')
+        // document.querySelector('.js-contact-form')
+        .addEventListener('submit', e => {
+            e.preventDefault();
+            new FormData(e.currentTarget).forEach((value, name) =>
+                console.log(`${name}: ${value}`),
+            );
+        });
+})();
