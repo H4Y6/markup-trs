eb48de4963c204e726abb9091d235903ab7bf3ad
h4y6 <h4y6@i.ua>
2023-09-06 16:44:31 +0300

    modified:   css/style-f.css modified:   forms.html new file:   img/img-c/check.svg modified:   js/log.js

--- js/log.js
+++ js/log.js
@@ -1,5 +1,5 @@
 (() => {
-    document.querySelector('.js-form-c')
+    document.querySelector('.js-form-a-checkbox')
         .addEventListener('submit', e => {
             e.preventDefault();
             new FormData(e.currentTarget).forEach((value, name) =>
